bb1d1f7ab7b75df1d8408ae007cf6d1ec087bea0
man lass <lassanaa14@gmail.com>
2022-04-07 05:56:07 -0600

    fix linters

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
 const menBtn = document.querySelector('.nae');
 const navstuff = document.querySelector('.mobile-menu');
 
-menBtn.addEventListener('click', function(event) {
-   menBtn.classList.toggle('navblur');
-    navstuff.classList.toggle('activate');
-   
+menBtn.addEventListener('click', () => {
+  menBtn.classList.toggle('navblur');
+  navstuff.classList.toggle('activate');
 });
-
-
-// const menuBtn = document.querySelector('.nav-hamburger');
-// const navlinks = document.querySelector('.nav-links');
-
-// menuBtn.addEventListener('click', () => {
-//     navlinks.classList.toggle('activin');
-//     menuBtn.classList.toggle('active');
-// });
