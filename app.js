afdbbaa3191204e7c2e103d6daacea3a9baec77f
man lass <lassanaa14@gmail.com>
2022-04-07 19:32:51 -0600

    js fix

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const menBtn = document.querySelector('.nae');
 const navstuff = document.querySelector('.mobile-menu');
+const Closer = document.querySelector('.close-button');
+const rep = document.getElementById('hide');
 
 menBtn.addEventListener('click', () => {
   menBtn.classList.toggle('navblur');
   navstuff.classList.toggle('activate');
   navstuff.classList.toggle('mobile-menu');
+  Closer.classList.toggle('visible');
+  rep.classList.toggle('invisible');
 });
