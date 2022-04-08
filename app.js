ae1ec3b911bd722b9e65cc3a61e584fc89436d3e
man lass <lassanaa14@gmail.com>
2022-04-07 20:51:45 -0600

    Made Cprrection to app.js

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const navstuff = document.querySelector('.mobile-menu');
 const Closer = document.querySelector('.close-button');
 const rep = document.getElementById('hide');
 
-menBtn.addEventListener('click', () => {
+rep.addEventListener('click', () => {
+  menBtn.classList.toggle('navblur');
+  navstuff.classList.toggle('activate');
+  navstuff.classList.toggle('mobile-menu');
+  Closer.classList.toggle('visible');
+  rep.classList.toggle('invisible');
+});
+Closer.addEventListener('click', () => {
   menBtn.classList.toggle('navblur');
   navstuff.classList.toggle('activate');
   navstuff.classList.toggle('mobile-menu');
